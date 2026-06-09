955642219ec8d15402f0c821d71d6339b32aeaa5
Téo Calvo <teocalvo2@gmail.com>
2026-06-09 10:14:09 -0300

    feat: login

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import dotenv
 dotenv.load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+EMAIL_AUTHORIZED = os.getenv("EMAIL_AUTHORIZED")
 
 from gen_ai import generate
 
@@ -75,82 +76,96 @@ st.set_page_config(page_title="Lista Inteligente")
 
 st.markdown("# Lista Inteligente!")
 
-produtos = get_produtos(engine)
+if not st.user.is_logged_in:
+    if st.button("Log in"):
+        st.login()
 
-try:
-    col, _ = st.columns(2)
-    numero_dias_adiante = col.number_input("Dias sem voltar ao mercado adiante",
-                                          min_value=1,
-                                          max_value=60,
-                                          step=1)
-    
-    df_stats = pd.read_sql(query, engine)
-    df_stats["comprar"] = df_stats["dias_ult_compra"] + numero_dias_adiante > df_stats["avg_diff_dias"]
-
-except Exception as err:
-    print(err)
-    df_stats = pd.DataFrame()
-
-
-if df_stats.empty:
-    st.warning("Não há dados históricos suficientes. Registre mais compras")
+elif st.user.email != EMAIL_AUTHORIZED:
+    st.warning("Usuário não autorizado. Entre com um email autorizado para acessar a aplicação.")
+    time.sleep(1)
+    st.logout()
 
 else:
-    show_df_compra(df_stats)
 
+    produtos = get_produtos(engine)
 
-st.markdown("## Adicionar Compras")
-tab_produto, tab_historico, tab_nf = st.tabs(["Produto", "Histórico", "Nota Fiscal"])
-
-
-with tab_produto:
-    
-    st.markdown("### Registrar Produto")
-    produto = st.selectbox("Produto", options=["Novo Produto"]+produtos)
-
-    if produto == "Novo Produto":
-        produto_novo = st.text_input("Inserir novo produto")
-        produto = produto_novo
-
-    valor = st.number_input("Valor", min_value=0.01)
-
-    if st.button("Registrar compra"):
-        data = {
-            "dt_compra":datetime.datetime.now().strftime("%Y-%m-%d"),
-            "produto": produto.title(),
-            "valor_produto": valor,
-        }
+    try:
+        col, _ = st.columns(2)
+        numero_dias_adiante = col.number_input("Dias sem voltar ao mercado adiante",
+                                            min_value=1,
+                                            max_value=60,
+                                            step=1)
         
-        df_insert = pd.DataFrame([data])
-        df_insert.to_sql("compras",engine, if_exists="append", index=False)
-        st.success("Compra do produto registrada com sucesso!")
+        df_stats = pd.read_sql(query, engine)
+        df_stats["comprar"] = df_stats["dias_ult_compra"] + numero_dias_adiante > df_stats["avg_diff_dias"]
 
-    
-with tab_historico:
-    st.markdown("### Importar Histórico")
-    open_file = st.file_uploader("Entre com um arquivo histórico", type="csv")
+    except Exception as err:
+        print(err)
+        df_stats = pd.DataFrame()
 
-    if open_file:
-        df = pd.read_csv(open_file)
-        df = st.data_editor(df)
+
+    if df_stats.empty:
+        st.warning("Não há dados históricos suficientes. Registre mais compras")
+
+    else:
+        show_df_compra(df_stats)
+
+
+    st.markdown("## Adicionar Compras")
+    tab_produto, tab_historico, tab_nf = st.tabs(["Produto", "Histórico", "Nota Fiscal"])
+
+
+    with tab_produto:
         
-        if st.button("Registrar Dados!"):
-            df.to_sql("compras", engine, if_exists="append", index=False)
-            st.success("Dados registrados com sucesso!")
-            time.sleep(1)
-            st.rerun()
+        st.markdown("### Registrar Produto")
+        produto = st.selectbox("Produto", options=["Novo Produto"]+produtos)
+
+        if produto == "Novo Produto":
+            produto_novo = st.text_input("Inserir novo produto")
+            produto = produto_novo
+
+        valor = st.number_input("Valor", min_value=0.01)
+
+        if st.button("Registrar compra"):
+            data = {
+                "dt_compra":datetime.datetime.now().strftime("%Y-%m-%d"),
+                "produto": produto.title(),
+                "valor_produto": valor,
+            }
+            
+            df_insert = pd.DataFrame([data])
+            df_insert.to_sql("compras",engine, if_exists="append", index=False)
+            st.success("Compra do produto registrada com sucesso!")
+
+        
+    with tab_historico:
+        st.markdown("### Importar Histórico")
+        open_file = st.file_uploader("Entre com um arquivo histórico", type="csv")
+
+        if open_file:
+            df = pd.read_csv(open_file)
+            df = st.data_editor(df)
+            
+            if st.button("Registrar Dados!"):
+                df.to_sql("compras", engine, if_exists="append", index=False)
+                st.success("Dados registrados com sucesso!")
+                time.sleep(1)
+                st.rerun()
 
 
-with tab_nf:
-    st.markdown("### Importar Nota Fiscal")
-    open_img = st.file_uploader("Entre com um arquivo de Nota Fiscal", type=["png", "jpeg"])
+    with tab_nf:
+        st.markdown("### Importar Nota Fiscal")
+        open_img = st.file_uploader("Entre com um arquivo de Nota Fiscal", type=["png", "jpeg"])
 
-    if open_img:
-        df = process_nf(prompt=prompt, resposta_template=resposta, produtos=produtos, img_file=open_img)
-        df = st.data_editor(df)
-        if st.button("Registrar Dados!"):
-            df.to_sql("compras", engine, if_exists="append", index=False)
-            st.success("Dados registrados com sucesso!")
-            time.sleep(1)
-            open_img = None
-            st.rerun()
+        if open_img:
+            df = process_nf(prompt=prompt, resposta_template=resposta, produtos=produtos, img_file=open_img)
+            df = st.data_editor(df)
+            if st.button("Registrar Dados!"):
+                df.to_sql("compras", engine, if_exists="append", index=False)
+                st.success("Dados registrados com sucesso!")
+                time.sleep(1)
+                open_img = None
+                st.rerun()
+
+    if st.button("Log Out"):
+        st.logout()
